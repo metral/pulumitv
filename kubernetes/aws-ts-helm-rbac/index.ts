90bc59ed7f5ee48b8607c2ffd78def368abd01f8
Mike Metral <1112768+metral@users.noreply.github.com>
2020-03-24 12:24:50 -0700

    Add init commit for aws-ts-helm-rbac

---
+++ kubernetes/aws-ts-helm-rbac/index.ts
@@ -0,0 +1,34 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as eks from "@pulumi/eks";
+import * as k8s from "@pulumi/kubernetes";
+import { FluentdCloudWatch } from "./fluentd-cloudwatch";
+
+const projectName = pulumi.getProject();
+
+// Create the EKS cluster.
+const cluster = new eks.Cluster(`${projectName}`, {
+    deployDashboard: false,
+    createOidcProvider: true,
+});
+
+// Export the kubeconfig.
+export const kubeconfig = cluster.kubeconfig;
+
+// Geet the cluster OIDC provider URL.
+if (!cluster?.core?.oidcProvider) {
+    throw new Error("Invalid cluster OIDC provider URL");
+}
+const clusterOidcProvider = cluster.core.oidcProvider;
+const clusterOidcProviderArn = clusterOidcProvider.arn;
+const clusterOidcProviderUrl = clusterOidcProvider.url;
+
+const provider = new k8s.Provider("provider", {kubeconfig: kubeconfig});
+
+// Deploy fluentd-cloudwatch Helm chart with pod IAM.
+const fluentd = new FluentdCloudWatch("fluentd-cloudwatch", {
+    provider: provider,
+    namespace: "kube-system",
+    clusterOidcProviderArn: clusterOidcProviderArn,
+    clusterOidcProviderUrl: clusterOidcProviderUrl,
+});
+export const fluentdCloudWatchLogGroupName = fluentd.logGroupName;
